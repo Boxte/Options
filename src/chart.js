becb4dca3512d2a284cc0ab5fd3b8a6da17c905a
vl <levane@vt.edu>
2022-10-31 16:06:14 -0700

    feat: added chart library

--- src/chart.js
+++ src/chart.js
@@ -1,3 +1,6 @@
+import "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
 var dataset1 = [
   [1, 1],
   [12, 20],
@@ -14,8 +17,31 @@ var dataset1 = [
   [100, 140],
 ];
 
-const Chart = (props) => {
-  return <div></div>;
+const LineGraph = (props) => {
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "First dataset",
+        data: [33, 53, 85, 41, 44, 65],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+      {
+        label: "Second dataset",
+        data: [33, 25, 35, 51, 54, 76],
+        fill: false,
+        borderColor: "#742774",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Line data={data}></Line>
+    </div>
+  );
 };
 
-export default Chart;
+export default LineGraph;
