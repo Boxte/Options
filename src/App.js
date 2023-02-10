acbb8d80770950b8eb09678dc0e42d98fa01a4cd
vl <levane@vt.edu>
2023-02-10 09:33:25 -0800

    feat: added in skeleton search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { ChakraProvider, Container } from "@chakra-ui/react";
-import LineGraph from "./LineGraph";
 import GraphsContainer from "./GraphsContainer";
-import { LongCall, LongPut, ShortCall, ShortPut } from "./options_data/Types";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <ChakraProvider>
       <Container w="100%" h="100%">
+        <SearchBar />
         <GraphsContainer />
       </Container>
     </ChakraProvider>
